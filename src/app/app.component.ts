f003e237b3d5eb8582dc4a4b40b3f0b32fc7257a
teslax <wiesieku1@gmail.com>
2023-04-27 00:52:55 +0200

    http client

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,40 @@
 import { Component } from '@angular/core';
 import { Route, ActivatedRoute, ParamMap } from '@angular/router';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'qmpl-ui';
-
-  constructor(private route: ActivatedRoute) {
-  }
+  private code = '';
+  constructor(private route: ActivatedRoute, private http: HttpClient) {}
 
   ngOnInit() {
-    this.route.queryParams.subscribe(params => {
-      if (params['code']){
-        console.log(params['code']);
+    this.route.queryParams.subscribe((params) => {
+      if (params['code']) {
+        this.code = params['code'];
+        console.log(this.code);
+
+        this.http.post(
+          'https://qmpl.auth.eu-central-1.amazoncognito.com/oauth2/token',
+          {
+            grant_type: 'authorization_code',
+            client_id:'3ojt0f6465n9i5hmffac81u9kj',
+            code:this.code,
+            redirect_uri:'https://teslax2.github.io/qmpl-ui'
+          },
+          {
+            headers: new HttpHeaders({
+              'Content-Type': 'application/x-www-form-urlencoded',
+              Authorization: 'Basic M29qdDBmNjQ2NW45aTVobWZmYWM4MXU5a2o',
+            }),
+          }
+        ).subscribe(x => console.log(x))
       }
     });
   }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { AppRoutingModule } from './app-routing.module';
   imports: [
     BrowserModule,
     NgbModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
