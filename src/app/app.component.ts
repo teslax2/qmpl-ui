05946e91f047c930760b9e04de6036b0715de180
teslax <wiesieku1@gmail.com>
2023-04-27 23:39:56 +0200

    fixed token fetching

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Route, ActivatedRoute, ParamMap } from '@angular/router';
+import { Route, ActivatedRoute, ParamMap, Router } from '@angular/router';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { catchError, retry } from 'rxjs/operators';
@@ -12,7 +12,7 @@ import { catchError, retry } from 'rxjs/operators';
 export class AppComponent {
   title = 'qmpl-ui';
   private code = '';
-  constructor(private route: ActivatedRoute, private http: HttpClient) {}
+  constructor(private route: ActivatedRoute, private http: HttpClient, private router: Router) {}
 
   ngOnInit() {
     this.route.queryParams.subscribe((params) => {
@@ -20,26 +20,32 @@ export class AppComponent {
         this.code = params['code'];
         console.log(this.code);
 
-        this.http.post(
-          'https://qmpl.auth.eu-central-1.amazoncognito.com/oauth2/token',
-          {
-            grant_type: 'authorization_code',
-            client_id:'3ojt0f6465n9i5hmffac81u9kj',
-            code:this.code,
-            redirect_uri:'https://teslax2.github.io/qmpl-ui'
-          },
-          {
-            headers: new HttpHeaders({
-              'Content-Type': 'application/x-www-form-urlencoded',
-              Authorization: 'Basic M29qdDBmNjQ2NW45aTVobWZmYWM4MXU5a2o',
-            }),
-          }
-        ).subscribe({
-          next: (data) => console.log(data),
-          error: (e) => console.error(e),
-          complete: () => console.info('complete'),
-        });
+        this.http
+          .post(
+            'https://qmpl.auth.eu-central-1.amazoncognito.com/oauth2/token',
+            {
+              grant_type: 'authorization_code',
+              client_id: '3ojt0f6465n9i5hmffac81u9kj',
+              code: this.code,
+              redirect_uri: 'https://teslax2.github.io/qmpl-ui',
+            },
+            {
+              headers: new HttpHeaders({
+                'Content-Type': 'application/x-www-form-urlencoded',
+                //Authorization: 'Basic M29qdDBmNjQ2NW45aTVobWZmYWM4MXU5a2o',
+              }),
+            }
+          )
+          .subscribe({
+            next: (data) => console.log(data),
+            error: (e) => console.error(e),
+            complete: () => console.info('complete'),
+          });
       }
     });
+  }
+
+  login() {
+    window.location.href = 'https://qmpl.auth.eu-central-1.amazoncognito.com/oauth2/authorize?response_type=code&client_id=3ojt0f6465n9i5hmffac81u9kj&redirect_uri=https://teslax2.github.io/qmpl-ui&scope=openid+email';
   }
 }
